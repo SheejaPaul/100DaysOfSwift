3f8f1526240111e9b7ee3937a9f3e31a4de83f59
Sheeja <shpaul@l-sff0v3htdd-m.gapinc.gap.com>
2020-01-09 14:59:16 -0800

    Day 1 of 100DaysOfSwift

---
+++ Day1/DoublesAndBooleans.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var str = "Hello, playground"
+var pi = 3.141
+var awesome = true
---
+++ Day1/MultilineStrings.swift
@@ -0,0 +1,24 @@
+//
+//  MultilineStrings.swift
+//  
+//
+//  Created by Sheeja Paul on 1/9/20.
+//
+
+import Foundation
+
+//Start and end with three double quote marks - output comes with line breaks
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+//To write multi-line strings without line breaks, end each line with a \, like this:
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
